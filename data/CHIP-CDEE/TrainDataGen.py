d5fd378b92401a3b0b7e24811e7f18afee23f81a
lzhu <lzhu@intersystems.com>
2024-06-07 16:02:50 +0800

    alpaca training data generated

--- data/CHIP-CDEE/TrainDataGen.py
+++ data/CHIP-CDEE/TrainDataGen.py
@@ -10,7 +10,6 @@ import os,json,codecs
 
 # 获取当前工作目录
 current_path = os.getcwd()
-trainingData = []
 
 promptText = """
 你是一个医疗行业数据提取助手。你能从从中文电子病历中挖掘出临床发现事件。即给定一段现病史或者医学影像所见报告，要求从中抽取临床发现事件的四个属性: 解剖部位、主体词、描述词，以及发生状态:
@@ -25,90 +24,6 @@ promptText = """
         tendency: 发生状态，字符串，如果没有发生状态，默认为""
         character: 描述词，列表结构，如果没有描述词，默认为[]
         anatomy_list: 解剖部位，列表结构，如果没有解剖部位，默认为[]
-例如，对这样一段文本描述：
-请外科会诊后建议至上级医院手术或者放化疗，但患者拒绝并办理出院；此后患者仍诉张口困难，偶有左侧面颈部轻微疼痛，无发热，无咳嗽咯痰，无腹痛腹泻，22天前患者在家属陪同下再次到我科住院治疗，入院后完善检查无化疗禁忌，征得患者及家属同意后于5-25日行紫杉醇240mg d1+奈达铂120mg d1化疗，并于5月29日给予帕米磷酸二钠抗溶骨治疗，治疗过程顺利，化疗后患者面部包块缩小，今日患者为行下一疗程化疗入院，门诊以“口腔粘膜恶性肿瘤 口腔鳞状细胞癌 t4n2M0”收治入院。
-你将使用这样的json记录临床发现事件：
-{
-    "id": 1425,
-    "text": "请外科会诊后建议至上级医院手术或者放化疗，但患者拒绝并办理出院；此后患者仍诉张口困难，偶有左侧面颈部轻微疼痛，无发热，无咳嗽咯痰，无腹痛腹泻，22天前患者在家属陪同下再次到我科住院治疗，入院后完善检查无化疗禁忌，征得患者及家属同意后于5-25日行紫杉醇240mg d1+奈达铂120mg d1化疗，并于5月29日给予帕米磷酸二钠抗溶骨治疗，治疗过程顺利，化疗后患者面部包块缩小，今日患者为行下一疗程化疗入院，门诊以“口腔粘膜恶性肿瘤 口腔鳞状细胞癌 t4n2M0”收治入院。",
-    "event": [
-      {
-        "core_name": "张口困难",
-        "tendency": "",
-        "character": [],
-        "anatomy_list": []
-      },
-      {
-        "core_name": "疼痛",
-        "tendency": "",
-        "character": [
-          "轻微"
-        ],
-        "anatomy_list": [
-          "左侧面",
-          "左侧颈部"
-        ]
-      },
-      {
-        "core_name": "发热",
-        "tendency": "否定",
-        "character": [],
-        "anatomy_list": []
-      },
-      {
-        "core_name": "咳嗽",
-        "tendency": "否定",
-        "character": [],
-        "anatomy_list": []
-      },
-      {
-        "core_name": "咳痰",
-        "tendency": "否定",
-        "character": [],
-        "anatomy_list": []
-      },
-      {
-        "core_name": "疼痛",
-        "tendency": "否定",
-        "character": [],
-        "anatomy_list": [
-          "腹"
-        ]
-      },
-      {
-        "core_name": "腹泻",
-        "tendency": "否定",
-        "character": [],
-        "anatomy_list": []
-      },
-      {
-        "core_name": "包块",
-        "tendency": "",
-        "character": [
-          "化疗后，缩小"
-        ],
-        "anatomy_list": [
-          "面部"
-        ]
-      },
-      {
-        "core_name": "恶性肿瘤",
-        "tendency": "",
-        "character": [],
-        "anatomy_list": [
-          "口腔粘膜"
-        ]
-      },
-      {
-        "core_name": "鳞癌",
-        "tendency": "",
-        "character": [],
-        "anatomy_list": [
-          "口腔"
-        ]
-      }
-    ]
-}
 不要提供任何解释，也不要返回任何无关文字，只返回回答变量。
 """
 
@@ -134,8 +49,10 @@ try:
 except json.JSONDecodeError as e:
     print("转换失败，文件内容可能不是有效的JSON格式。错误信息：", e)
 
-conversationItem=[]
-for i in range(len(data)):
+trainingData = []
+
+#In ShareGPT format
+""" for i in range(len(data)):
     text = data[i].get('text')
     event = data[i].get('event')
     human = {}
@@ -144,10 +61,22 @@ for i in range(len(data)):
     gpt={}
     gpt["from"] = "gpt"
     gpt["value"] = json.dumps(event, ensure_ascii=False, indent=4)
+    conversationItem=[]
     conversationItem.append(human)
     conversationItem.append(gpt)
+    trainingData.append({"conversations": conversationItem,"system":promptText}) """
     
-trainingData.append({"conversations": conversationItem,"system":promptText})
+#In alpaca format
+for i in range(len(data)):
+    text = data[i].get('text')
+    event = data[i].get('event')
+    conversation = {}
+    conversation["instruction"]="请将下列医疗文本转换为以json格式标注的临床发现事件："
+    conversation["input"]=text
+    conversation["output"]=json.dumps(event, ensure_ascii=False, indent=4)
+    conversation["system"]=promptText
+    trainingData.append(conversation)
+
 
 # 将Python字典转换为JSON格式的字符串
 json_data = json.dumps(trainingData, ensure_ascii=False, indent=4)
